f3c80b1bf9f9919dc931ecd31b8179d25faf2855
chichi1410 <hoatkhtk2004@gmail.com>
2024-08-20 19:21:54 +0700

    test git

---
+++ InRaThongTinCaNhan.java
@@ -0,0 +1,19 @@
+public class InRaThongTinCaNhan{
+     
+    public static void main(String[] args){
+       String name;
+       float height;
+       int age;
+       name = "nguyen dai hoa";
+       height = 1.64f;
+       age = 20;
+       System.out.print("name:");// print ko xuong dong con println la cxuong dong
+       System.out.println(name);
+       System.out.print("height: ");
+       System.out.println(height);
+       System.out.print("age: ");
+       System.out.println(age);
+
+    }
+
+}
